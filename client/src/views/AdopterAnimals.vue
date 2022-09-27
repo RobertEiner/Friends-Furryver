45b7a31d4d1e78eb8a031041785895fbb2c44d09
luiz <lphilippirosane@gmail.com>
2022-09-27 23:57:03 +0200

    Create adopter view

---
+++ client/src/views/AdopterAnimals.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <navbar-adopter
+      firstTab="Animals"
+      secondTab="Applications"
+      thirdTab="Profile"
+    />
+  </div>
+</template>
+
+<script>
+import { Api } from '@/Api'
+import NavbarAdopter from '@/components/NavbarAdopter.vue'
+
+export default {
+  name: 'adopter',
+  components: {
+    'navbar-adopter': NavbarAdopter
+  },
+
+  mounted() {
+    Api.get('/animals').then((response) => {
+      this.animals = response.data.Animals
+    })
+  },
+
+  data() {
+    return {
+      animals: []
+    }
+  }
+}
+</script>
