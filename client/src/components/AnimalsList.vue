1dffec38f04ce23f8d61f2d3a98e6dfd157a6dce
luiz <lphilippirosane@gmail.com>
2022-09-27 23:55:31 +0200

    Create list component for the animals cards

---
+++ client/src/components/AnimalsList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <b-container fluid>
+      <b-row align-h="center">
+        <b-colum sm :key="animal._id" v-for="animal in animals">
+          <animal-card :animal="animal"> </animal-card>
+        </b-colum>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import { Api } from '@/Api'
+import AnimalCard from '@/components/AnimalCard.vue'
+
+export default {
+  name: 'animals-list',
+  components: {
+    'animal-card': AnimalCard
+  },
+  mounted() {
+    Api.get('/animals')
+      .then((response) => {
+        this.animals = response.data.Animals
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  },
+  data() {
+    return {
+      animals: []
+    }
+  }
+}
+</script>
