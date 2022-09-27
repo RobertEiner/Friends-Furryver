35b093dd80a7d50aa0e0f9820e5f382a98e58a33
luiz <lphilippirosane@gmail.com>
2022-09-27 23:54:02 +0200

    Create animal card for adopter page

---
+++ client/src/components/AnimalCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="margin">
+    <b-card
+      title="Animal"
+      img-src="https://picsum.photos/600/300/?image=25"
+      img-alt="Image"
+      img-top
+      tag="article"
+      style="max-width: 20rem"
+    >
+      <b-list>
+        <b-list-group-item> Size: {{ animal.size }} </b-list-group-item>
+        <b-list-group-item> Species: {{ animal.species }} </b-list-group-item>
+        <b-list-group-item> Breed: {{ animal.breed }} </b-list-group-item>
+        <b-list-group-item>
+          Personality: {{ animal.personality }}
+        </b-list-group-item>
+        <b-list-group-item>Gender: {{ animal.gender }}</b-list-group-item>
+        <b-list-group-item>Age: {{ animal.age }}</b-list-group-item>
+        <b-list-group-item>Hours needed: {{ animal.hours }}</b-list-group-item>
+      </b-list>
+
+      <b-button variant="success" class="applyButton">Apply</b-button>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'animal-card',
+  props: ['animal']
+}
+</script>
+
+<style scoped>
+.margin {
+  padding: 3% 7%;
+}
+.applyButton {
+  padding: 3%;
+}
+</style>
