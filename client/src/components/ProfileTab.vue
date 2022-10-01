a7d74034ddb25fe7791202b29912338365854d34
Robert <guseinero@student.gu.se>
2022-10-01 22:54:35 +0200

    Add initial profile tab

---
+++ client/src/components/ProfileTab.vue
@@ -0,0 +1,19 @@
+<template>
+  <b-container>
+    <b-row>
+        <b-col>
+            <p>PROFILE TAB</p>
+        </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'profile-tab'
+}
+</script>
+
+<style>
+
+</style>
