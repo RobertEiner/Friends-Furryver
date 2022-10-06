05323977b4d7b061b6920cbcf1982e81ae2df0f1
Robert <guseinero@student.gu.se>
2022-10-06 22:46:08 +0200

    Add functinoality to display processed applications

--- client/src/components/ApplicationTab.vue
+++ client/src/components/ApplicationTab.vue
@@ -1,63 +1,142 @@
 <template>
-  <b-container>
-    <b-row>
-      <b-col>
-        <b-card
-          title="Application"
-          img-src="https://picsum.photos/600/300/?image=25"
-          img-alt="Image"
-          img-top
-          tag="article"
-          style="max-width: 20rem;"
+  <b-tabs pills>
+    <b-tab title="Active applications">
+      <div v-if="activeApplications.length > 0">
+        <span
+          v-for="activeApplication in activeApplications"
+          :key="activeApplication._id"
         >
-          <b-list-group-item>
-            Application ID: {{ application._id }}
-          </b-list-group-item>
-
-          <b-list-group-item>
-            Adopter ID: {{ application.adopter }}
-          </b-list-group-item>
-          <b-list-group-item>
-            Animal ID: {{ application.animal }}
-          </b-list-group-item>
-          <b-list-group-item>
-            Application status: {{ application.status }}
-          </b-list-group-item>
-
-          <b-button
-            id="accept-application-button"
-            size="sm"
-            variant="primary"
-            @click="$emit('accept-application', application._id)"
-            >Accept application</b-button
+          <b-card
+            class="active-card"
+            title="Application"
+            img-src="https://picsum.photos/600/300/?image=25"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 20rem;"
           >
+            <b-list-group-item>
+              Application ID: {{ activeApplication._id }}
+            </b-list-group-item>
 
-          <b-button
-            id="decline-application-button"
-            size="sm"
-            variant="primary"
-            @click="$emit('del-application', application._id)"
-            >Decline application</b-button
+            <b-list-group-item>
+              Adopter ID: {{ activeApplication.adopter }}
+            </b-list-group-item>
+            <b-list-group-item>
+              Animal ID: {{ activeApplication.animal }}
+            </b-list-group-item>
+            <b-list-group-item>
+              Application status: {{ activeApplication.status }}
+            </b-list-group-item>
+            <application-accept></application-accept>
+            <b-button
+              id="accept-application-button"
+              size="sm"
+              variant="primary"
+              @click="$emit('accept-application', activeApplication._id)"
+              >Accept application</b-button
+            >
+
+            <b-button
+              id="decline-application-button"
+              size="sm"
+              variant="primary"
+              @click="$emit('del-application', activeApplication._id)"
+              >Decline application</b-button
+            >
+          </b-card>
+        </span>
+      </div>
+      <div v-else>
+        <p class="no-active-applications">Currently no active applications</p>
+      </div>
+    </b-tab>
+
+    <b-tab title="Processed applications">
+      <div v-if="handledApplications.length > 0">
+        <span
+          v-for="handledApplicationsTwo in handledApplications"
+          :key="handledApplicationsTwo._id"
+        >
+          <b-card
+            class="handled-card"
+            title="Application"
+            img-src="https://picsum.photos/600/300/?image=25"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 20rem;"
           >
-        </b-card>
-      </b-col>
-    </b-row>
-  </b-container>
+            <b-list-group-item>
+              Application ID: {{ handledApplicationsTwo._id }}
+            </b-list-group-item>
+
+            <b-list-group-item>
+              Adopter ID: {{ handledApplicationsTwo.adopter }}
+            </b-list-group-item>
+            <b-list-group-item>
+              Animal ID: {{ handledApplicationsTwo.animal }}
+            </b-list-group-item>
+            <b-list-group-item>
+              Application status: {{ handledApplicationsTwo.status }}
+            </b-list-group-item>
+            <b-button
+              class="remove-application-button"
+              size="sm"
+              variant="primary"
+              @click="$emit('remove-application', handledApplicationsTwo._id)"
+              >Remove application</b-button
+            >
+          </b-card>
+        </span>
+      </div>
+
+      <div v-else>
+        <p class="no-processed-applications">
+          Currently no processed applications
+        </p>
+      </div>
+    </b-tab>
+  </b-tabs>
 </template>
 
 <script>
 // import { Api } from '@/Api'
+
 export default {
   name: 'applications-tab',
-  props: ['application'],
+  components: {},
+  props: ['applications'],
   data() {
-    return {}
+    return {
+      activeApplications: [],
+      handledApplications: []
+    }
   },
-  methods: {}
+  created() {
+    this.sortApplications()
+  },
+  methods: {
+    sortApplications() {
+      for (let i = 0; i < this.applications.length; i++) {
+        if (this.applications[i].status === 0) {
+          this.activeApplications.push(this.applications[i])
+        } else {
+          this.handledApplications.push(this.applications[i])
+        }
+      }
+    }
+
+  }
 }
 </script>
 
 <style scoped>
+.remove-application-button {
+  margin: 2% auto;
+  background-color: red;
+}
+
 #accept-application-button {
   margin: 2% auto;
 
@@ -66,5 +145,25 @@ export default {
 
 #decline-application-button {
   background-color: red;
+}
+
+.active-card {
+  display: inline-block;
+  margin: 2%;
+}
+
+.handled-card {
+  display: inline-block;
+  margin: 1%;
+}
+
+.no-active-applications {
+  margin: 10%;
+  padding-bottom: 17%;
+}
+
+.no-processed-applications {
+  margin: 10%;
+  padding-bottom: 20%;
 }
 </style>
--- client/src/views/AdoptionCenter.vue
+++ client/src/views/AdoptionCenter.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-container fluid>
+  <b-container class="outer-container" fluid>
     <b-row>
       <b-col>
         <div class="inner-div">
@@ -7,14 +7,15 @@
           <b-tabs content-class="mt-3" class="tabs">
             <b-tab title="Pets" active>
               <div>
-                <b-card-group deck>
-                  <div v-for="animal in animals" v-bind:key="animal._id">
+                <div>
+                  <span class="animals-span" v-for="animal in animals" v-bind:key="animal._id">
                     <pets-tab
                       v-bind:animal="animal"
                       v-on:del-animal="deleteAnimal"
                     />
-                  </div>
-                </b-card-group>
+                  </span>
+                </div>
+
                 <b-button
                   class="add-animal-button"
                   v-on:click="goToAddAnimal()"
@@ -27,22 +28,22 @@
 
             <b-tab title="Applications">
               <div>
-                <b-card-group deck>
-                  <div
-                    v-for="application in applications"
-                    :key="application._id"
-                  >
-                    <applications-tab
-                      :application="application"
-                      v-on:del-application="declineApplication"
-                      v-on:accept-application="acceptApplication"
-                    ></applications-tab>
-                  </div>
-                </b-card-group>
+                <applications-tab
+                  :applications="applications"
+                  v-if="applications.length > 0"
+                  v-on:del-application="declineApplication"
+                  v-on:accept-application="acceptApplication"
+                  v-on:remove-application="deleteApplication"
+                ></applications-tab>
               </div>
             </b-tab>
-
-            <b-tab title="Profile"> <profile-tab></profile-tab> </b-tab>
+            <div>
+              <b-tab title="Profile">
+                <profile-tab
+                  :adoptionCenter="adoptionCenter"
+                ></profile-tab>
+              </b-tab>
+            </div>
           </b-tabs>
         </div>
       </b-col>
@@ -65,45 +66,73 @@ export default {
     'applications-tab': ApplicationTab,
     ProfileTab
   },
+
   mounted() {
     this.getAllAnimals()
     this.getAllApplications()
+    this.getAdoptionCenter()
   },
   data() {
     return {
       animals: [],
-      applications: []
+      applications: [],
+      adoptionCenter: {}
+
     }
   },
   methods: {
     goToAddAnimal() {
       this.$router.push(`/adoptionCenters/${this.$route.params.id}/addAnimal`)
     },
+    // Delete an animal from the adoption center
     deleteAnimal(id) {
-      Api.delete(`/animals/${id}`).then(response => {
-        const index = this.animals.findIndex(animal => animal._id === id)
-        this.animals.splice(index, 1)
-      }).catch(error => {
-        console.log(error)
-      })
+      Api.delete(`/animals/${id}`)
+        .then(response => {
+          console.log(id)
+          const index = this.animals.findIndex(animal => animal._id === id)
+          this.animals.splice(index, 1)
+        })
+        .catch(error => {
+          console.log(error)
+        })
     },
+    // Accept an adoption application, which changes the status code to 1, which means accepted
     acceptApplication(applicationId) {
-      Api.put(`/adoption-applications/${applicationId}`, { status: 1 }).then(response => {
-        console.log(response.data)
-      }).catch(error => {
-        console.log(error)
-      })
+      Api.put(`/adoption-applications/${applicationId}`, { status: 1 })
+        .then(response => {
+          console.log(response.data)
+        })
+        .catch(error => {
+          console.log(error)
+        })
     },
+
+    // declines an adoption application, changing the status code to 2, which means declined
     declineApplication(applicationId) {
-      Api.delete(`/adoption-applications/${applicationId}`).then(response => {
-        const index = this.applications.findIndex(
-          application => application._id === applicationId
-        )
-        this.applications.splice(index, 1)
-      }).catch(error => {
-        console.log(error)
-      })
+      Api.put(`/adoption-applications/${applicationId}`, { status: 2 })
+        .then(response => {
+          console.log(response)
+        })
+        .catch(error => {
+          console.log(error)
+        })
     },
+
+    // Deletes an adoption application
+    deleteApplication(applicationId) {
+      Api.delete(`/adoption-applications/${applicationId}`)
+        .then(response => {
+          const index = this.applications.findIndex(
+            application => application._id === applicationId
+          )
+          this.applications.splice(index, 1)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+
+    // Get all the animals of an doption center
     getAllAnimals() {
       Api.get(`/adoptionCenters/${this.$route.params.id}/animals`)
         .then(response => {
@@ -114,11 +143,23 @@ export default {
           console.log(error)
         })
     },
+    // Get all applications of an adoption center
     getAllApplications() {
       Api.get(`/AdoptionCenters/${this.$route.params.id}/adoptionApplications`)
         .then(response => {
           console.log(response.data)
           this.applications = response.data.Applications
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    // Get a specific adoption center
+    getAdoptionCenter() {
+      Api.get(`/AdoptionCenters/${this.$route.params.id}`)
+        .then(response => {
+          this.adoptionCenter = response.data
+          console.log(response.data)
         })
         .catch(error => {
           console.log(error)
@@ -131,9 +172,18 @@ export default {
 <style scoped>
 .inner-div {
   background-color: aliceblue;
+  margin-bottom: 0;
+}
+
+.outer-container {
+  background-color: rgb(119, 200, 246);
 }
 
 .add-animal-button {
   margin: 2%;
+}
+
+.animals-span {
+  display: inline-block;
 }
 </style>
