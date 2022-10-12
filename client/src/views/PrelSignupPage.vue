b8adbbb7be609fc3001ae67cbfaeb4d3e7243836
Luiz Rosane <lphilippirosane@gmail.com>
2022-10-12 17:11:53 +0200

    Format PrelSignupPage document

--- client/src/views/PrelSignupPage.vue
+++ client/src/views/PrelSignupPage.vue
@@ -1,65 +1,67 @@
 <template>
   <div>
-      <body>
-        <home-login-signup-style></home-login-signup-style>
-        <div class="container px-4 py-5 mx-auto">
-    <div class="card card0">
-        <div class="d-flex flex-lg-row flex-column-reverse">
+    <body>
+      <home-login-signup-style></home-login-signup-style>
+      <div class="container px-4 py-5 mx-auto">
+        <div class="card card0">
+          <div class="d-flex flex-lg-row flex-column-reverse">
             <div class="card card1">
-                <div class="row justify-content-center my-auto">
-                    <div class="col-md-8 col-10 my-5">
-                        <div class="row justify-content-center px-3 mb-3">
-                            <img id="logo" src='../Assets/pet-house.png'>
-                        </div>
-                        <h3 class="mb-5 text-center heading">Sign up</h3>
+              <div class="row justify-content-center my-auto">
+                <div class="col-md-8 col-10 my-5">
+                  <div class="row justify-content-center px-3 mb-3">
+                    <img id="logo" src="../Assets/pet-house.png" />
+                  </div>
+                  <h3 class="mb-5 text-center heading">Sign up</h3>
 
-                        <h6 class="msg-info">Please create an account</h6>
+                  <h6 class="msg-info">Please create an account</h6>
 
-                        <div class="row justify-content-center my-3 px-3">
-                        <b-button
-                                    class="btn-block btn-color"
-                                    @click="goToAdoptionCenterSignup"
-                                    >Register as Adoption Center
-                        </b-button>
-                        </div>
+                  <div class="row justify-content-center my-3 px-3">
+                    <b-button
+                      class="btn-block btn-color"
+                      @click="goToAdoptionCenterSignup"
+                      >Register as Adoption Center
+                    </b-button>
+                  </div>
 
-                        <div class="row justify-content-center my-3 px-3">
-                        <b-button
-                                    class="btn-block btn-color"
-                                    @click="goToAdopterSignup"
-                                    >Register as Adopter
-                        </b-button>
-                        </div>
-
-                    </div>
+                  <div class="row justify-content-center my-3 px-3">
+                    <b-button
+                      class="btn-block btn-color"
+                      @click="goToAdopterSignup"
+                      >Register as Adopter
+                    </b-button>
+                  </div>
                 </div>
+              </div>
 
-                        <div class="bottom text-center mb-5">
-                            <p href="#"
-                                        class="sm-text mx-auto mb-3"
-                                        >Already have an account?
-                                <b-button
-                                            class="btn btn-white ml-2"
-                                            @click="goToHome"
-                                            >Login
-                                </b-button>
-                            </p>
-                        </div>
+              <div class="bottom text-center mb-5">
+                <p href="#" class="sm-text mx-auto mb-3">
+                  Already have an account?
+                  <b-button class="btn btn-white ml-2" @click="goToHome"
+                    >Login
+                  </b-button>
+                </p>
+              </div>
             </div>
             <div class="card card2">
-                <div class="row mx-2">
-                  <h3 class="text-black justify-content-center pt-5 pb-5">We are more than just a company</h3>
-                </div>
-                <!-- <div class="row mx-auto mb-5 "></div> -->
-                <div class ="row mx-auto mb-5">
-                  <small class="text-black">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    </small>
-                </div>
+              <div class="row mx-2">
+                <h3 class="text-black justify-content-center pt-5 pb-5">
+                  We are more than just a company
+                </h3>
+              </div>
+              <!-- <div class="row mx-auto mb-5 "></div> -->
+              <div class="row mx-auto mb-5">
+                <small class="text-black"
+                  >Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </small>
+              </div>
             </div>
+          </div>
         </div>
-    </div>
-</div>
-      </body>
+      </div>
+    </body>
   </div>
 </template>
 
