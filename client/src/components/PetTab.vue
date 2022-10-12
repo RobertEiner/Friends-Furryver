ce263af7c54c03ae4a78e99e1fe8ba9cb1cbe269
Robert <guseinero@student.gu.se>
2022-10-12 14:28:42 +0200

    Remove breed from animal card and add display message when there are no animals in adoption center

--- client/src/components/PetTab.vue
+++ client/src/components/PetTab.vue
@@ -1,56 +1,58 @@
 <template>
   <b-container>
     <b-row>
-      <b-col>
-        <div v-if="animals.length > 0">
-          <span v-for="animal in animals" :key="animal._id">
-            <b-card
-              class="animal-card"
-              title="Animal"
-              img-src="https://picsum.photos/600/300/?image=25"
-              img-alt="Image"
-              img-top
-              tag="article"
-              style="max-width: 20rem;"
+      <b-col  v-if="animals.length > 0">
+        <span v-for="animal in animals" :key="animal._id">
+          <b-card
+            class="animal-card"
+            title="Animal"
+            img-src="https://picsum.photos/600/300/?image=25"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 20rem;"
+          >
+            <b-list-group-item>
+              Species: {{ animal.species }}
+            </b-list-group-item>
+
+            <b-list-group-item> Size: {{ animal.size }} </b-list-group-item>
+
+            <b-list-group-item>
+              Health status: {{ animal.healthStatus }}
+            </b-list-group-item>
+            <b-list-group-item>
+              Personality: {{ animal.personality }}
+            </b-list-group-item>
+            <b-list-group-item>Gender: {{ animal.gender }}</b-list-group-item>
+            <b-list-group-item>Age: {{ animal.age }}</b-list-group-item>
+            <b-list-group-item
+              >Hours needed: {{ animal.hours }}</b-list-group-item
             >
-              <b-list-group-item>
-                Species: {{ animal.species }}
-              </b-list-group-item>
-              <b-list-group-item> Breed: {{ animal.breed }} </b-list-group-item>
 
-              <b-list-group-item> Size: {{ animal.size }} </b-list-group-item>
-
-              <b-list-group-item>
-                Health status: {{ animal.healthStatus }}
-              </b-list-group-item>
-              <b-list-group-item>
-                Personality: {{ animal.personality }}
-              </b-list-group-item>
-              <b-list-group-item>Gender: {{ animal.gender }}</b-list-group-item>
-              <b-list-group-item>Age: {{ animal.age }}</b-list-group-item>
-              <b-list-group-item
-                >Hours needed: {{ animal.hours }}</b-list-group-item
-              >
-
-              <b-button
-                class="update-animal-button"
-                size="sm"
-                v-on:click="goToUpdateAnimal(animal._id)"
-                variant="primary"
-                >Update animal</b-button
-              >
-              <b-button
-                class="delete-animal-button"
-                size="sm"
-                v-on:click="$emit('del-animal', animal._id)"
-                variant="primary"
-                >Delete animal</b-button
-              >
-            </b-card>
-          </span>
-        </div>
-        <div v-else>
-          <h1>Currently there are no animals in your adoption center</h1>
+            <b-button
+              class="update-animal-button"
+              size="sm"
+              @click="goToUpdateAnimal(animal._id)"
+              variant="primary"
+              >Update animal</b-button
+            >
+            <b-button
+              class="delete-animal-button"
+              size="sm"
+              @click="$emit('del-animal', animal._id)"
+              variant="primary"
+              >Delete animal</b-button
+            >
+          </b-card>
+        </span>
+      </b-col>
+      <b-col v-else sm="12">
+        <div>
+          <h5 class="no-animals-title">
+            Currently, there are no animals in your adoption center
+          </h5>
+          <b-img class="no-animals-img" :src="require('../Assets/pet-house.png')" fluid></b-img>
         </div>
       </b-col>
     </b-row>
@@ -89,5 +91,13 @@ export default {
   margin: 2% auto;
   font-size: 1em;
   display: block;
+}
+
+.no-animals-title {
+  padding-bottom: 1%;
+}
+
+.no-animals-img {
+  max-width: 15rem;
 }
 </style>
