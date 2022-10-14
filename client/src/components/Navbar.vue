a4ee18404ca00a31bffd4b8ae551f5723d5eb66c
Robert <guseinero@student.gu.se>
2022-10-14 16:11:46 +0200

    Add log out button and media query to put it underneath the navbar brand when screen size < 334px

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,33 +1,70 @@
 <template>
   <b-navbar type="dark">
     <b-img
-            class="dog-img-navbar"
-            :src="require('../Assets/pet-house.png')"
-            rounded="top"
-            fluid-grow
-          ></b-img>
-    <b-navbar-brand class="navbar" >
-
-        Friends Furryver
+      class="dog-img-navbar"
+      :src="require('../Assets/pet-house.png')"
+      rounded="top"
+      fluid-grow
+    ></b-img>
+    <b-navbar-brand class="navbar">
+      Friends Furryver
     </b-navbar-brand>
+    <b-button
+      id="log-out-button"
+      class="ml-auto"
+      pill
+      variant="outline-secondary"
+      @click="logUserOut()"
+      >Log out</b-button
+    >
   </b-navbar>
 </template>
 
 <script>
 export default {
-  name: 'navbar'
+  name: 'navbar',
+  methods: {
+    logUserOut() {
+      localStorage.removeItem('jwt')
+      this.$router.push('/')
+    }
+  }
 }
 </script>
 
 <style scoped>
-    .navbar {
-        background-color: rgb(58, 172, 213);
-        font-family: 'Pacifico', cursive;
-    }
+.navbar {
+  background-color: rgb(58, 172, 213);
+  font-family: 'Pacifico', cursive;
 
-    .dog-img-navbar {
-      max-width: 2rem;
-    }
+}
 
-    @import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
+.dog-img-navbar {
+  max-width: 2rem;
+}
+
+#log-out-button {
+  color: white;
+  border-color: white;
+
+}
+
+#log-out-button:hover {
+  background-color: rgb(79, 196, 239);
+}
+
+@media (max-width: 334px) {
+  .navbar {
+
+  display: inline-block;
+
+}
+
+#log-out-button {
+
+  margin: 2% 0;
+}
+}
+
+@import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
 </style>
