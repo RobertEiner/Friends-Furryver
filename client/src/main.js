28d6c2c3a727df009a275b079ee8ec29f5289bc8
Robert <guseinero@student.gu.se>
2023-01-31 14:22:58 +0100

    Test comment

--- client/src/main.js
+++ client/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import BootstrapVue from 'bootstrap-vue'
-
+//add comment
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
