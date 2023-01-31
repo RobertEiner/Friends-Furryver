3f9c20c32449b96c108160940083b082b33f3723
Robert <guseinero@student.gu.se>
2023-01-31 14:38:26 +0100

    add comment

--- client/src/main.js
+++ client/src/main.js
@@ -5,7 +5,7 @@ import BootstrapVue from 'bootstrap-vue'
 //add comment
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-
+//add comment
 Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
